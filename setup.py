dc3c01c73d22302d2a58595108ee742b2c67032e
JD Black <black.jondavid@gail.com>
2024-06-11 23:29:33 -0500

    allow for some interpretation and flexibility in the final rating rather than perfection

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_dependencies = [
 ]
 
 setup(
-    version="0.2.9",
+    version="0.2.10",
     name="aac-req-qa",
     license="MIT License",
     url="https://github.com/DevOps-MBSE/AaC-Req-QA",

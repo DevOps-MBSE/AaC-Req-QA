24593568d12ad4a9a3dab0b610937a3d1e8cff29
JD Black <black.jondavid@gail.com>
2024-06-11 22:18:54 -0500

    bump version to publish correctly

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_dependencies = [
 ]
 
 setup(
-    version="0.2.8",
+    version="0.2.9",
     name="aac-req-qa",
     license="MIT License",
     url="https://github.com/DevOps-MBSE/AaC-Req-QA",
